75c1cc5931b5c3e66aa1bc3fc05edee8c7f7eaff
Nimrod Tzeler <nimrod.zeler@gmail.com>
2020-05-23 17:59:30 +0300

    add multiple ways of getting exe path

--- GetPathTest/Program.cs
+++ GetPathTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace GetPathTest
 {
@@ -6,7 +8,28 @@ namespace GetPathTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(File.Exists(@".\Results.txt"))
+            {
+                File.Delete(@".\Results.txt");
+            }
+            WriteResult("\nEnvironment.GetCommandLineArgs()[0]", Environment.GetCommandLineArgs()[0]);
+            WriteResult("\nSystem.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0])", System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]));
+            WriteResult("\nDirectory.GetCurrentDirectory()", Directory.GetCurrentDirectory());
+            WriteResult("\nEnvironment.CurrentDirectory", Environment.CurrentDirectory);
+            WriteResult("\nSystem.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)", System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            WriteResult("\nAppDomain.CurrentDomain.BaseDirectory", AppDomain.CurrentDomain.BaseDirectory);
+            WriteResult("\nSystem.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)", System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
+            WriteResult("\nSystem.AppContext.BaseDirectory", System.AppContext.BaseDirectory);
+            WriteResult("\nAppDomain.CurrentDomain.BaseDirectory", AppDomain.CurrentDomain.BaseDirectory);
+            WriteResult("\nSystem.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)", System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName));
+        }
+
+        static void WriteResult(string type, string result)
+        {
+            using (StreamWriter file = new StreamWriter(@".\Results.txt", true))
+            {
+                file.WriteLine($"{type} result: {result}");
+            }
         }
     }
 }
